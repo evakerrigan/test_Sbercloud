b67de16dcea33efc82f1b57333870460bdb07d85
Eva Kerrigan <evakerrigan@gmail.com>
2023-06-15 03:01:13 +0300

    feat: add formik and structure main form

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,56 @@
 import "./Main.css";
+import { Formik, Form, Field } from "formik";
+
+function validatePhone(value: string) {
+  if (!value) {
+    return "Введите номер телефона";
+  } else if (value.length !== 11) {
+    return "Номер телефона должен состоять из 11 цифр";
+  }
+}
+function validateEmail(value: string) {
+  if (!value) {
+    return "Введите email";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return "Неверный email";
+  }
+}
 
 export const Main = () => {
   return (
     <div className="main">
+      <Formik
+        initialValues={{
+          phone: "",
+          email: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ touched, errors }) => (
 
+          <Form className="form">
+
+            <label className="form__description">Номер телефона</label>
+            <Field type="phone" name="phone" placeholder="Phone" validate={validatePhone} />
+            {touched.phone && errors.phone && (
+              <div className="form__error">{errors.phone}</div>
+            )}
+
+            <label className="form__description">Email</label>
+            <Field type="email" name="email" placeholder="Email" validate={validateEmail} />
+            {touched.email && errors.email && (
+              <div className="form__error">{errors.email}</div>
+            )}
+
+            <button type="submit" className="form__button">Начать</button>
+
+          </Form>
+
+        )}
+
+      </Formik>
     </div>
   )
 }
