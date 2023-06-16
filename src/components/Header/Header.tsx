e300a18b6871a8b0a34f3af7997f1f4ed7ee992a
Eva Kerrigan <evakerrigan@gmail.com>
2023-06-17 00:23:38 +0300

    feat: add my name at header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="header">
       <div className="header__avatar"><span>АИ</span></div>
       <div className="header__wrapper">
-        <h2 className="header__name">Иван Иванов</h2>
+        <h2 className="header__name">Ксения Любасова</h2>
         <ul className="header__list">
           <li className="header__item">
             <a href="https://t.me/EvaKerrigan" className="header__link" target="_blank">Telegram</a>
