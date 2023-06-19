9245d183006375ffa5695dc4fe24af90ee123bb0
Eva Kerrigan <evakerrigan@gmail.com>
2023-06-19 08:37:20 +0300

    refactor: divide modal

---
+++ src/components/Modal/ErrorModal.tsx
@@ -0,0 +1,24 @@
+import './Modal.css';
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+export const Modal = ({onClose }: ModalProps) => {
+  return (
+    <div className="modal">
+      <div className="modal__content error">
+        <div className="modal__title">Ошибка</div>
+        <div className="modal__icon-wrapper">
+          <div className="modal__icon error"></div>
+        </div>
+        <button id="button-close" className="form__button next" onClick={onClose}>
+          Закрыть
+        </button>
+        <div className="modal__close" onClick={onClose}></div>
+      </div>
+    </div>
+  );
+
+
+};
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -20,20 +20,4 @@ export const Modal = ({onClose }: ModalProps) => {
     </div>
   );
 
-  return (
-    <div className="modal">
-      <div className="modal__content error">
-        <div className="modal__title">Ошибка</div>
-        <div className="modal__icon-wrapper">
-          <div className="modal__icon error"></div>
-        </div>
-        <button id="button-close" className="form__button next" onClick={onClose}>
-          Закрыть
-        </button>
-        <div className="modal__close" onClick={onClose}></div>
-      </div>
-    </div>
-  );
-
-
 };
